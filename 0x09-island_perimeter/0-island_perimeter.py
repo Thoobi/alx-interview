c49af085ed329cd2358d44031e39d06211723cae
Thoobi <sydneydaniel76@gmail.com>
2025-01-16 12:47:39 +0100

    adds another changes to the perimter file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-
-"""
-Island Perimeter Problem
+"""Island Perimeter Problem
+This script contains a function that calculates the perimeter of an island
+in a grid, where 1 represents land and 0 represents water.
 """
 
 
 def island_perimeter(grid):
     """
-    Calculates the perimeter of the island described in grid
+    Calculates the perimeter of the island described in the grid.
+
     Args:
-        grid: 2d list of integers containing 0(water) or 1(land)
-    Return:
-        the perimeter of the island
+        grid (list of list of int): A 2D grid where 1 represents land
+            and 0 represents water.
+
+    Returns:
+        int: The perimeter of the island.
+
     """
-
-    p = 0
+    perimeter = 0
     for i in range(len(grid)):
-
         for j in range(len(grid[i])):
-
-            if (grid[i][j] == 1):
-
-                if (i <= 0 or grid[i - 1][j] == 0):
-                    p += 1
-                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    p += 1
-                if (j <= 0 or grid[i][j - 1] == 0):
-                    p += 1
-                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    p += 1
-    return p
-
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
